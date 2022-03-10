40e836275352fe5f2a038bc378fe33c2f12c6ddf
dmitry <dmitrym2019@yandex.ru>
2022-03-10 16:09:09 +0300

    fix_calc

--- script.js
+++ script.js
@@ -126,79 +126,122 @@
 // }
 
 
+console.log('hi');
 // Задание 9_________________________________
 
-// function calculatator(){
+function calculatator(){
 
-// for (i=1;i<10;i++){
-//     btn = document.createElement('button');
-//     btn.classList.add('button')
-//     btn.setAttribute('id',`${i}`);
-//     btn.textContent = `${i}`
-//     document.body.append(btn);
-// }
-// signes = ["*","/","+","-"];
-// for (i=0;i<signes.length;i++){
-//     btn = document.createElement('button');
-//     btn.classList.add('button');
-//     btn.setAttribute('id',`${signes[i]}`);
-//     btn.textContent = `${signes[i]}`;
-//     document.body.append(btn);
-// }
+divCalc = document.createElement('div');
+divCalc.classList.add('divCalc');
+document.body.append(divCalc);
+CalcDiv = document.querySelector('.divCalc');
 
-// btnRes = document.createElement('button');
-// btnRes.classList.add('button');
-// btnRes.setAttribute('id','result')
-// result = document.querySelector('#result');
-// btnRes.textContent = 'результат'
-// document.body.append(btnRes);
+str = document.createElement('p');
+str.classList.add('str');
+stroka = document.querySelector('.str');
+CalcDiv.appendChild(str);
+CalcDiv.appendChild
 
-// inp = document.createElement('input');
-// inp.classList.add('inp');
-// input = document.querySelector('.inp');
-// document.body.append(inp);
-
-// str = document.createElement('p');
-// str.classList.add('str');
-// stroka = document.querySelector('.str');
-// document.body.append(str);
-
-// dlt = document.createElement('button');
-// dlt.classList.add('dlt');
-// document.body.append(dlt);
-// delet = document.querySelector('.dlt');
-// dlt.textContent = "стереть все";
+signs = document.createElement('div');
+signs.classList.add('signs');
+CalcDiv.append(signs);
+CalcSigns = document.querySelector('.signs');
 
 
-// delet.addEventListener('click', ()=>{
-//     str.textContent = "";
-// })
+div123 = document.createElement('div');
+div123.classList.add('div123');
+CalcDiv.append(div123);
+Calc123 = document.querySelector('.div123');
+
+div456 = document.createElement('div');
+div456.classList.add('div456');
+CalcDiv.append(div456);
+Calc456 = document.querySelector('.div456');
+
+div789 = document.createElement('div');
+div789.classList.add('div789');
+CalcDiv.append(div789);
+Calc789 = document.querySelector('.div789');
+
+divZeroRes = document.createElement('div');
+divZeroRes.classList.add('divZeroRes');
+CalcDiv.append(divZeroRes);
+CalcZeroRes = document.querySelector('.divZeroRes');
+
+signes = ["*","/","+","-"];
+for (i=0;i<signes.length;i++){
+    btn = document.createElement('button');
+    btn.classList.add('button');
+    btn.setAttribute('id',`${signes[i]}`);
+    btn.textContent = `${signes[i]}`;
+    document.body.append(btn);
+
+    CalcSigns.appendChild(btn);
+    
+}
+
+for (i=0;i<10;i++){
+    btn = document.createElement('button');
+    btn.classList.add('button')
+    btn.setAttribute('id',`${i}`);
+    btn.textContent = `${i}`
+    document.body.append(btn);
+     if (i!=0 && i<=3){   
+        Calc123.appendChild(btn);
+    }
+     if (i!=0 && i>3 && i<=6){   
+        Calc456.appendChild(btn);
+    }
+    if (i!=0 && i>6 && i<=10){   
+        Calc789.appendChild(btn);
+    }
+    if (i==0){   
+        CalcZeroRes.appendChild(btn);
+    }
+
+}
+
+dlt = document.createElement('button');
+dlt.classList.add('dlt');
+CalcZeroRes.appendChild(dlt);
+delet = document.querySelector('.dlt');
+dlt.textContent = "стереть все";
+
+btnRes = document.createElement('button');
+btnRes.classList.add('button');
+btnRes.setAttribute('id','result')
+result = document.querySelector('#result');
+btnRes.textContent = 'результат'
+CalcDiv.append(btnRes);
+
+delet.addEventListener('click', ()=>{
+    str.textContent = "";
+})
 
 
-// btnRes.addEventListener('click',()=>{
-//     alert(`${str.textContent} = ${eval(str.textContent)}`);
-//     str.textContent = eval(str.textContent) ;
-// })
+btnRes.addEventListener('click',()=>{
+    alert(`${str.textContent} = ${eval(str.textContent)}`);
+    str.textContent = eval(str.textContent) ;
+})
 
-// document.addEventListener('click', function(event,equation){
-//     targ = (event.target).getAttribute('id');
-//     if (targ != null && targ != 'result'){
-//         console.log(targ);
-//         console.log(str.textContent);
-//         str.innerText += targ;
-//     }
-// })
+document.addEventListener('click', function(event,equation){
+    targ = (event.target).getAttribute('id');
+    if (targ != null && targ != 'result'){
+        console.log(targ);
+        console.log(str.textContent);
+        str.innerText += targ;
+    }
+})
 
-// btnRes.addEventListener('click', function(){
-//     console.log(str[0]);
-// })
+btnRes.addEventListener('click', function(){
+    console.log(str[0]);
+})
 
 
-// buttons = document.querySelectorAll('.button');
-// buttons.addEventListener('click', function(event){
-// console.log(event.target);
-// })
 
-// }
-// calculatator();
+}
+calculatator();
+
+
+
 
